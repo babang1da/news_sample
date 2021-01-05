916f3c9827fba0591c7463c044f6db0edca9e78b
babang1da <pr45opra@gmail.com>
2021-01-05 21:35:47 +0300

    Initial Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Egor Oprea on 05.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
